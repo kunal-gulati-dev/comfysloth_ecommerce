fc5cb1076103c515a37239894b3bfad3f42b3ec1
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-06 15:17:54 +0530

    added the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className='section-center'>
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus earum nulla et ea dicta nemo vero magnam veniam ad quidem.</p>
+        <form className='contact-form'>
+          <input type="email" className='form-input' placeholder='Enter Email' />
+          <button type='submit' className='submit-btn'>Subscribe</button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,29 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>Custom Furniture <br />Built only for you.</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ab adipisci quae voluptatum assumenda, explicabo iure earum esse? Mollitia, eligendi!</p>
+
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const {id, icon, title, text} = service
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
