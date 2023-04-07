3463eee32302490b29aebc43b4cfe415fbba6a3f
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-07 14:51:07 +0530

    added the single product page functionality

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,7 +18,11 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  feature_products:[]
+  feature_products:[],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
+
 }
 
 const ProductsContext = React.createContext()
@@ -42,6 +46,17 @@ export const ProductsProvider = ({ children }) => {
       dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
     } catch (error) {
       dispatch({type: GET_PRODUCTS_ERROR})
+    }
+  }
+
+  const fetchSigleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct});
+    }catch(erro) {
+      dispatch({type: GET_SINGLE_PRODUCT_ERROR})
     }
   }
 
