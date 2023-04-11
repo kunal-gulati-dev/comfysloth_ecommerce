07f12d7e816c34837a71dc49b6f3ee77bb9b5d54
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-11 18:15:31 +0530

    added the cart functinality

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -43,7 +43,7 @@ export const CartProvider = ({ children }) => {
 
   // toggle amount
   const toggleAmount = (id, value) => {
-
+    dispatch({type: TOGGLE_CART_ITEM_AMOUNT, payload: {id, value}})
   }
 
   // clear cart
@@ -52,6 +52,7 @@ export const CartProvider = ({ children }) => {
   }
 
   useEffect(() => {
+    dispatch({type: COUNT_CART_TOTALS})
     localStorage.setItem('cart', JSON.stringify(state.cart))
   }, [state.cart])
 
